0a244765a8f3ecbd1b510f59b5b3c6aa8fdbc6a5
Gary.Yang <gary.yang@hy-tech.com.tw>
2020-11-30 10:31:49 +0800

    update

--- stock_Kline.py
+++ stock_Kline.py
@@ -2,6 +2,10 @@ import requests
 import json
 import time
 from multiprocessing import Pool
+from itertools import repeat
+from functools import partial
+from find2 import Ui_SecWindow
+import threading
 
 requests.packages.urllib3.disable_warnings()
 
@@ -39,6 +43,22 @@ getallStockQuery = {
     'cmkey': ''
 }
 
+getchipHeader = {
+    "accept": "application/json, text/plain, */*",
+    "x-system-kind": "FUND_OLD_DRIVER",
+    "x-platform": "WEB, WEB",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36",
+    "x-cnyes-app": "unknown",
+    "origin": "https://invest.cnyes.com",
+    "sec-fetch-site": "same-site",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-dest": "empty",
+    "referer": "https://invest.cnyes.com/",
+    "accept-encoding": "gzip, deflate, br",
+    "accept-language": "zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7",
+    "pragma": "no-cache",
+    "cache-control": "no-cache"
+}
 
 def getCmKey():
     getAllStockHeader['Referer'] = 'https://www.cmoney.tw/finance/f00025.aspx?s=3515'
@@ -54,6 +74,7 @@ def getCmKey():
 def getAllStock(pool):
     print("取得所有股票代碼")
     urls = []
+    index = 0
     getAllStockHeader['Referer'] = 'http://www.cmoney.tw/finance/f00025.aspx'
     r = requests.get('http://www.cmoney.tw/finance/ashx/mainpage.ashx',
                      params=getallStockQuery, headers=getAllStockHeader, verify=False)
@@ -66,7 +87,8 @@ def getAllStock(pool):
             else:
                 pool.map(getKline, urls)
                 urls = []
-
+        index += 1
+        
 # 判斷紅黑K
 def checkRedBlackK(data):
     # 紅K return true
@@ -101,13 +123,13 @@ def crossK(data):
     # 紅K情境
     if (checkRedBlackK(data)):
         # 必須要有上下影線
-        if (data['h'] == data['c'] or data['l'] == data['o']): return False
+        # if (data['h'] == data['c'] or data['l'] == data['o']): return False
         # 實體線小於0.5%
         if ((data['c'] - data['o']) / data['o'] < 0.005): return True
     # 黑K情境
     else:
         # 必須要有上下影線
-        if (data['h'] == data['o'] or data['l'] == data['c']): return False
+        # if (data['h'] == data['o'] or data['l'] == data['c']): return False
         # 實體線小於0.5%
         if ((data['o'] - data['c']) / data['c'] < 0.01): return True
     return False
@@ -130,7 +152,7 @@ def redEatBlack(id, data, day):
     secLastKline['v'] = data['v'][len(data['v'])-day-1]
     if (checkRedBlackK(secLastKline) and not checkRedBlackK(lastKline) and secLastKline['o'] < lastKline['c']
             and secLastKline['c'] > lastKline['o'] and lastKline['v'] > 2000):
-        print(id, lastKline, secLastKline)
+        print(id)
 
 # 流星(看跌)
 def metor(id, data, day):
@@ -178,30 +200,67 @@ def tandemK(id, data, day):
     # 先判斷前一天是不是大紅K
     if (bigRedK(secLastKline) is False): return False
     # 判斷當天跳空和十字線
-    if (secLastKline['c'] < lastKline['o'] and crossK(lastKline)): 
+    if (secLastKline['c'] <= lastKline['o'] and crossK(lastKline)): 
         print(id)
+
+# 投信突擊
+def itAttack(data, day):
+    day -= 1
+    checkBuy = False
+    if len(data) < 3 + day :
+        return False
+    # 近三天投信有沒有大買
+    for i in range(3):
+        if data[len(data)-1-i-day] >= 700:
+            checkBuy = True
+        elif data[len(data)-1-i-day] < 700 and checkBuy == False:
+            checkBuy = False
+    if checkBuy == False :
+        return False
+
+    # 抓前三天以前到近3個月的資料開始看投信是不是都沒動作
+    count = 0
+    while count < 60 if len(data) > 60 else len(data):
+        if data[len(data)-4-count-day] > 700: 
+            return False
+        count += 1
+    return True
+
 
 def getKline(id):
     query['symbol'] = id
-    try:
-        r = requests.get('https://histock.tw/Stock/tv/udf.asmx/history',
-                         params=query, headers=getKlineHeader, verify=False)
-        data = json.loads(r.text)
-        # 當天 = 1，昨天 = 2...
-        day = 1
-        # [o]開、[h]高、[l]低、[c]收、[v]量
-        # if(data['s'] == 'ok' and len(data['o']) > 50 and checkUp(data, day)):
-        #     redEatBlack(id, data, day)
-        if(data['s'] == 'ok' and len(data['o']) > 50 ):
-            tandemK(id, data, day)
-    except Exception as e:
-        print('有點問題',id, e)
+    # 當天 = 1，昨天 = 2...
+    day = 1
+    method = '投信突擊'
+    if ( method == '投信突擊'):
+        try:
+            r = requests.get('https://marketinfo.api.cnyes.com/mi/api/v1/TWS:' + str(id) + ':STOCK/investors',
+                  headers=getchipHeader, verify=False)
+            data = json.loads(r.text)
+            if data['data']['datasets'] == None :
+                return
+            data = data['data']['datasets']['it']
+            if itAttack(data, day):
+                print(id)
+        except Exception as e:
+            print('有點問題',id, e)
+    else :
+        try:
+            r = requests.get('https://histock.tw/Stock/tv/udf.asmx/history',
+                            params=query, headers=getKlineHeader, verify=False)
+            data = json.loads(r.text)
+            # [o]開、[h]高、[l]低、[c]收、[v]量
+            if (method == '紅K吞噬黑K' and data['s'] == 'ok' and len(data['o']) > 50 and checkUp(data, day)):
+                redEatBlack(id, data, day)
+            elif (method == '串聯K' and data['s'] == 'ok' and len(data['o']) > 50 and data['v'][len(data['v'])-day] > 1000 ):
+                tandemK(id, data, day)
+        except Exception as e:
+            print('有點問題',id, e)
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     pool = Pool()
     getCmKey()
-    # getKline('1305')
     getAllStock(pool)
     pool.close()
     pool.join()
+
